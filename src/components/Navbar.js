ddba16dbca9f04738d320a450bdf053f6a36c29e
Do Thien Hoang <theksbd@gmail.com>
2022-06-03 13:36:47 +0700

    create a button to a navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import '../styles/Navbar.css';
+import ReorderIcon from '@material-ui/icons/Reorder';
 
 function Navbar() {
+    const [expandNavBar, setExpandNavBar] = useState(false);
+
+    const location = useLocation();
+
+    useEffect(() => {
+        setExpandNavBar(false);
+    }, [location])
+
     return (
-        <div className="navbar">
+        <div className="navbar" id={expandNavBar ? "open" : "close"}>
             <div className="toggleButton">
-                <button></button>
+                <button onClick={() => setExpandNavBar(prev => !prev)}>
+                    <ReorderIcon />
+                </button>
             </div>
             <div className="links">
                 <Link to="/">Home</Link>
