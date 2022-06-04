aa9b4ad8792883a9da98266c6e7c0ee2cca0b547
Do Thien Hoang <theksbd@gmail.com>
2022-06-04 20:57:47 +0700

    complete building project page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import '../styles/Projects.css';
 import ProjectItem from '../components/ProjectItem';
-// import Img
+import { ProjectList } from '../helpers/ProjectList';
 
 function Projects() {
     return (
         <div className='projects'>
             <h1>My personal projects</h1>
             <div className="projectList">
-                <ProjectItem name="Social Media Website" image={}/>
-                <ProjectItem />
+                {
+                    ProjectList.map((project, index) => {
+                        return <ProjectItem id={index} name={project.name} image={project.image} />
+                    })
+                }
             </div>
         </div>
     );
