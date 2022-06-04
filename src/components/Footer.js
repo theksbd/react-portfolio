f80c6e623e2685b53ebfbfd0f0de2e31831136ec
Do Thien Hoang <theksbd@gmail.com>
2022-06-04 15:22:00 +0700

    change order of social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../styles/Footer.css';
 import FacebookIcon from '@material-ui/icons/Facebook';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GithubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 
 function Footer() {
     return (
         <div className='footer'>
             <div className="socialMedia">
                 <a href="https://www.facebook.com/kids.the.5/" target="_blank" rel="noreferrer"><FacebookIcon /></a>
-                <a href="https://www.linkedin.com/in/ho%C3%A0ng-%C4%91%E1%BB%97-thi%E1%BB%87n-4b77b8228/" target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a>
                 <a href="https://github.com/theksbd" target="_blank" rel="noopener noreferrer"><GithubIcon /></a>
+                <a href="https://www.linkedin.com/in/ho%C3%A0ng-%C4%91%E1%BB%97-thi%E1%BB%87n-4b77b8228/" target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a>
             </div>
             <p>&copy; 2022 Do Thien Hoang - theksbd</p>
         </div>
